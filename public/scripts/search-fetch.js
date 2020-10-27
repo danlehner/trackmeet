761a6f42b06e64d5087996066e94e913d67ac476
danlehner <lehnerdr@gmail.com>
2020-10-27 15:53:15 -0400

    creates chained fetch methods to pull back Deezer data

--- public/scripts/search-fetch.js
+++ public/scripts/search-fetch.js
@@ -13,13 +13,42 @@ const GET_CONFIG = {
 const dzSearch = (query) => {
   return fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=track:"${query}"`, GET_CONFIG)
    .then(res => res.json())
-   .then(json => console.log(json))
+   .then(json => {
+     json.data.map(song => {
+      const artistId = song.artist.id
+      const songTitle = song.title
+      const artistName = song.artist.name
+      const albumArt = song.album.cover_big
+      const albumId = song.album.id
+
+      return fetch(`https://deezerdevs-deezer.p.rapidapi.com/album/${albumId}`, GET_CONFIG)
+        .then(res => res.json())
+        .then(album => {
+          
+          let genreId
+          if (album.genre_id !== -1) {
+            genreId = album.genre_id
+          } else {
+            genreId = 0
+          }
+
+          return fetch(`https://deezerdevs-deezer.p.rapidapi.com/genre/${genreId}`, GET_CONFIG)
+          .then(res => res.json())
+          .then(genre => {
+            const genreName = genre.name
+
+            console.log(artistId, songTitle, artistName, albumArt, genreName, genreId)
+          })
+      })
+   })
+ })
 }
 
-$(document).ready(() => {
-  $('.result-box').on('click', () => {
-    console.log('test')
-  })
+$('.result-box').submit(function(e) {
+  e.preventDefault() 
+  const searchVal = $('.results-input').val()
+  dzSearch(searchVal)
 })
+
 
 
