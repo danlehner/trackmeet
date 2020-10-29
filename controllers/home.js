fb9bbf18adf89dec094aa0ff93e5d9420fb830c4
danlehner <lehnerdr@gmail.com>
2020-10-28 20:20:38 -0400

    wraps search post in a trycatch

--- controllers/home.js
+++ controllers/home.js
@@ -15,40 +15,47 @@ router.get('/search', (req, res) => {
 
 // TEST SEARCH POST ROUTE
 router.post('/search', async (req, res) => {
-  const foundArtist = await db.Artist.findOne({ dzArtistId: req.body.dzArtistId})
-  const foundGenre = await db.Genre.findOne({ dzGenreId: req.body.dzGenreId })
 
-  // if (foundArtist) {
-
-  // }
-
-  // if (foundGenre) {
-
-  // }
-
-  const createdGenre = await db.Genre.create({
-    dzGenreId: req.body.dzGenreId,
-    name: req.body.genre
-  })
-
-  req.body.genre = createdGenre
-
-  const createdArtist = await db.Artist.create({
-    dzArtistId: req.body.dzArtistId,
-    name: req.body.artist,
-    genre: req.body.genre
-  })
-
-  req.body.artist = createdArtist
+  try {
+    const foundArtist = await db.Artist.findOne({ dzArtistId: req.body.dzArtistId})
+    const foundGenre = await db.Genre.findOne({ dzGenreId: req.body.dzGenreId })
   
-  const createdSong = await db.Song.create(req.body)
-
-  createdGenre.songs.push(createdSong)
-  createdGenre.artists.push(createdArtist)
-  createdArtist.songs.push(createdSong)
-
-  await createdArtist.save()
-  await createdGenre.save()
+    // if (foundArtist) {
+  
+    // }
+  
+    // if (foundGenre) {
+  
+    // }
+  
+    const createdGenre = await db.Genre.create({
+      dzGenreId: req.body.dzGenreId,
+      name: req.body.genre
+    })
+  
+    req.body.genre = createdGenre
+  
+    const createdArtist = await db.Artist.create({
+      dzArtistId: req.body.dzArtistId,
+      name: req.body.artist,
+      genre: req.body.genre
+    })
+  
+    req.body.artist = createdArtist
+    
+    const createdSong = await db.Song.create(req.body)
+  
+    createdGenre.songs.push(createdSong)
+    createdGenre.artists.push(createdArtist)
+    createdArtist.songs.push(createdSong)
+  
+    await createdArtist.save()
+    await createdGenre.save()
+    
+  } catch (error) {
+    console.log(error)
+    res.send( { message: 'Internal Server Error'} )
+  }
 })
 
 module.exports = router
