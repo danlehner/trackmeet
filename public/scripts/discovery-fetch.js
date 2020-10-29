e2e110c47c9cfb0735041710e4cb08fe5a2a49df
danlehner <lehnerdr@gmail.com>
2020-10-28 21:24:18 -0400

    skels out the discovery index page and adds head info

---
+++ public/scripts/discovery-fetch.js
@@ -0,0 +1,11 @@
+console.log('sanity check')
+
+const DZ_API_KEY = 'fae0f6f90emsh8e90cd79d598afcp1e6185jsn8b369fe30354'
+
+const GET_CONFIG = {
+	"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+			"x-rapidapi-key": `${DZ_API_KEY}`
+		}
+}
