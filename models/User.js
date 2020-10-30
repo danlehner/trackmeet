aadb334ac24490d402502b027ebaec4e0ae598da
danlehner <lehnerdr@gmail.com>
2020-10-30 12:37:42 -0400

    creates music models for user

--- models/User.js
+++ models/User.js
@@ -4,6 +4,25 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true }, 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, 
+  profilePic: { type: String }, 
+  genres: [
+    {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Genre'
+    }
+  ], 
+  artists: [
+    {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Artist'
+    }
+  ], 
+  songs: [
+    {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Song'
+    }
+  ]
 })
 
 const User = mongoose.model("User", userSchema)
