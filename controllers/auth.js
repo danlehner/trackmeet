a1ea32153f7e196bb3b8ec00ddc99ea9aa326360
danlehner <lehnerdr@gmail.com>
2020-10-30 13:01:52 -0400

    adds registration and login logic to auth

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const router = express.Router() 
+const db = require('../models')
+const bcrypt = require('bcryptjs')
+
+router.get('/register', (req, res) => {
+  res.render('auth/register')
+})
+
+router.post('/register', async (req, res) => {
+  try {
+
+    const foundUser = await db.User.findOne({ email: req.body.email })
+    
+    if (foundUser) {
+      res.send({ message: "Account already exists"})
+    }
+
+    const salt = await bcrypt.genSalt(10)
+
+    const hash = await bcrypt.hash(req.body.password, salt)
+
+    req.body.password = hash
+    
+    await db.User.create(req.body)
+
+    res.redirect('/login')
+
+  } catch (error) {
+    console.log(error)
+    res.send({ message: "Internal Service Error"})
+  }
+})
+
+router.get('/login', (req, res) => {
+  res.send('this is the login page')
+})
+
+router.post('/login', async (req, res) => { 
+  try {
+    const foundUser = await db.User.findOne({ email: req.body.email })
+
+    if (!foundUser) {
+      return res.send({ message: "Email or password is incorrect"})
+    }
+
+    const match = await bcrypt.compare(req.body.password, foundUser.password)
+
+    if (!match) { 
+      return res.send({ message: "Email or password is incorrect"})
+    }
+
+    req.session.currentUser = {
+      username: foundUser.username, 
+      id: foundUser._id
+    }
+
+    res.redirect('/profile')
+    
+  } catch (error) {
+    console.log(error)
+    res.send({ message: "Internal Service Error"})
+  }
+})
+
+router.delete('/logout', async (req, res) => {
+  await req.session.destroy() 
+  res.redirect('/')
+})
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const path = require('path');
+const session = require('express-session'); 
+const MongoStore = require('connect-mongo')(session)
 
 // Internal references
 const controllers = require('./controllers'); 
@@ -16,10 +18,28 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
 app.use(methodOverride("_method")); 
 app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(session({
+  resave: false, 
+  saveUninitialized: false, 
+  secret: "spacewafflefighter", 
+  store: new MongoStore({ 
+    url: process.env.MONGODB_URL || "mongodb://localhost:27017/trackstar"
+  }), 
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}))
 
+const authRequired = function(req, res, next) {
+  if(!req.session.currentUser) {
+    return res.redirect("/login")
+  }
+  next(); 
+}
 
 /* CONTROLLER ROUTING */
 
+// set up app.get for '/' once some of the other issues have been taken care of
 app.use('/', controllers.home)
 app.use('/profile', controllers.profile)
 app.use('/discovery', controllers.discovery)
