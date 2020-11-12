ec9d8417949435ec1dd0802219da5198801bda72
danlehner <lehnerdr@gmail.com>
2020-11-12 11:28:46 -0500

    sets dynamic port listener

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 // Internal references
 const controllers = require('./controllers'); 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs'); 
 
