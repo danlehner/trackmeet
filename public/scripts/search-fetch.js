1c2bc7f4ea7321bf9fd723b8a66f37932d9f1541
danlehner <lehnerdr@gmail.com>
2020-11-12 14:38:08 -0500

    revert back to public Deezer API key

--- public/scripts/search-fetch.js
+++ public/scripts/search-fetch.js
@@ -1,3 +1,4 @@
+const DZ_API_KEY = "fae0f6f90emsh8e90cd79d598afcp1e6185jsn8b369fe30354"
 
 const GET_CONFIG = {
 	"method": "GET",
@@ -74,7 +75,6 @@ const dzSearch = (query) => {
             }
 
             $('#loading-text').remove() 
-
 
             displayResults(artistId, songTitle, artistName, artistPicture, albumArt, genreName, genrePicture, genreId, cardId); 
 
